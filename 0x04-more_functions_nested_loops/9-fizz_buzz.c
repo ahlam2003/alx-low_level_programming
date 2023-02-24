192d43c809ca4849de5552e45b0df4497612e403
ahlam2003 <ahlamelaitari1@gmail.com>
2023-02-23 18:33:55 -0800

    ahlam

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,32 +1,34 @@
 #include <stdio.h>
-#include "main.h"
-
 /**
- * main - Enrty
- *
- * Description: prints the numbers 1 - 100 w/
- * Fizz for multiples of 3, Bizz for multiple of 5,
- * and FizzBizz for multiple of both
- *
- * Return: always 0
- */
+*main - program that prints either number
+*or fizz or buzz or fizzBuzz
+*
+*
+*Return: returns 0
+*/
 int main(void)
 {
-	int i;
+int num;
+while (num++ < 100)
 
-	for (i = 0; i <= 100; i++)
-	{
-		if (i % 15 == 0)
-			printf("FizzBuzz");
-		else if (i % 3 == 0)
-			printf("Fizz");
-		else if (i % 5 == 0)
-			printf("Bizz");
-		else
-			printf("%i", i);
-		if (i < 100)
-			printf(" ");
-	}
-	printf("\n");
-	return (0);
+if ((num % 3 == 0) && (num % 5 == 0))
+printf("FizzBuzz ");
+
+else if ((num % 3) == 0)
+printf("Fizz ");
+
+else if ((num % 5) == 0)
+{
+if (num != 100)
+printf("Buzz ");
+
+else
+printf("Buzz");
 }
+
+else
+printf("%d ", num);
+
+printf("\n");
+return (0);
+}}
