3922e2572c9de113fad81a75628ee68c39ce545b
ahlam2003 <ahlamelaitari1@gmail.com>
2023-03-01 11:59:16 -0800

    ahlam

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,27 +1,26 @@
-#include "main.h"
-
+#include "holberton.h"
 /**
- * rot13 - encodes a string into rot13
- * @s: string to encode
- *
- * Return: address of s
+ * rot13 - encodes a string using rot13.
+ * @str: pointer to string
+ * Return: the pointer to the string
  */
-char *rot13(char *s)
+
+char *rot13(char *str)
 {
 	int i, j;
 	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
-	for (i = 0; *(s + i); i++)
+	for (i = 0; *(str + i); i++)
 	{
 		for (j = 0; j < 52; j++)
 		{
-			if (a[j] == *(s + i))
+			if (a[j] == *(str + i))
 			{
-				*(s + i) = b[j];
+				*(str + i) = b[j];
 				break;
 			}
 		}
 	}
-	return (s);
+	return (str);
 }
