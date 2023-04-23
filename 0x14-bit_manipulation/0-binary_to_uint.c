aa71551127517815e715e9edec76a9c6017c0644
ahlam2003 <ahlamelaitari1@gmail.com>
2023-04-22 17:27:06 -0700

    ahlam

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+unsigned int binary_to_uint(const char *b)
+	{
+	unsigned int num = 0;
+	int i;
+
+	if (!b)
+		return (0);
+
+	for (i = 0; b[i] != '\0'; i++)
+		{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+
+	}
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		num <<= 1;
+		if (b[i] == '1')
+			num += 1;
+	}
+	return (num);
+
+}
