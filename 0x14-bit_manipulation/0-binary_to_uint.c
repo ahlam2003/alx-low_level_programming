031534aa0f6d8cde1aa2b5c271e91dac5afbebfb
ahlam2003 <ahlamelaitari1@gmail.com>
2023-05-15 13:35:54 -0700

    0x14

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,24 +1,15 @@
-#include "holberton.h"
+#include "main.h"
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0;
-	int i;
+		unsigned int number = 0;
+			int i = 0;
 
-	if (!b)
-		return (0);
-
-	for (i = 0; b[i] != '\0'; i++)
-	{
-		if (b[i] != '0' && b[i] != '1')
-			return (0);
-
-	}
-	for (i = 0; b[i] != '\0'; i++)
-	{
-		num <<= 1;
-		if (b[i] == '1')
-			num += 1;
-	}
-	return (num);
-
+				if (b == 0)
+							return (0);
+					for (i = 0 ; (b[i] == '0') || (b[i] == '1'); i++)
+							{
+										number  <<= 1;
+												number += b[i] - '0';
+													}
+						return (number);
 }
