819c2282f270c43b2125cb240e1c3cb3a61c2de8
ahlam2003 <ahlamelaitari1@gmail.com>
2023-05-19 11:20:48 -0700

    ahlam

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,36 +1,48 @@
-#include "holberton.h"
+#include "main.h"
+
+/**
+ * read_textfile - a function that reads a text file and prints it
+ *                to POSIX standard output.
+ *
+ * @filename: is the file to read
+ * @letters: number of letters to read and print from file
+ *
+ * Return: 0 if it fails or actual number of letters it could
+ *         read and print
+*/
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
+	int file;
+	ssize_t read_check, wcount;
 	char *buffer;
-	ssize_t read_counter, write_counter = 0;
 
-	if (filename == NULL)
+	if (filename == NULL) /*check if file is present*/
 		return (0);
-	buffer = malloc(letters);
+
+	file = open(filename, O_RDONLY); /*open file*/
+
+	if (file == -1)
+		return (0);
+
+	/*get the size of buffer from number of letters*/
+	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
-		return (0);
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
 	{
 		free(buffer);
 		return (0);
 	}
-	read_counter = read(fd, buffer, letters);
-	if (read_counter == -1)
-	{
-		free(buffer);
-		close(fd);
+
+	read_check = read(file, buffer, letters); /*read file*/
+	if (read_check == -1) /*check if read failed*/
 		return (0);
-	}
-	write_counter = write(STDOUT_FILENO, buffer, read_counter);
-	if (write_counter == -1)
-	{
-		free(buffer);
-		close(fd);
+
+	wcount = write(STDOUT_FILENO, buffer, read_check); /*write to POSIX*/
+	if (wcount == -1 || read_check != wcount) /*check if write failed*/
 		return (0);
-	}
-	close(fd);
+
 	free(buffer);
-	return (write_counter);
+
+	close(file); /*close file*/
+
+	return (wcount);
 }
